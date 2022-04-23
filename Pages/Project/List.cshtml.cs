a44712fc147c38a3aecfafca43be1f99c45809fc
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2022-04-23 18:38:58 -0500

    Creat Blogs

---
+++ Pages/Project/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortFolio.Pages
+{
+    public class ListProjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
